5ba5080c8078310693e03eb1ef8d4acff5d54cff
Lloyd Ramey <lnr0626@gmail.com>
2017-02-12 00:04:44 -0500

    Switch to ToStringSerializer

--- base-models/src/main/kotlin/com/lloydramey/cfn/model/ParameterTypes.kt
+++ base-models/src/main/kotlin/com/lloydramey/cfn/model/ParameterTypes.kt
@@ -1,26 +1,20 @@
 package com.lloydramey.cfn.model
 
-import com.fasterxml.jackson.core.JsonGenerator
-import com.fasterxml.jackson.databind.SerializerProvider
 import com.fasterxml.jackson.databind.annotation.JsonSerialize
-import com.fasterxml.jackson.databind.ser.std.StdSerializer
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer
 
-@JsonSerialize(using = ParameterTypeSerializer::class)
-sealed class ParameterType(internal val serialization: String) {
+@JsonSerialize(using = ToStringSerializer::class)
+sealed class ParameterType(private val serialization: String) {
     data class Single(val type: String) : ParameterType(type)
     data class List(val contents: Single) : ParameterType("List<${contents.type}>")
+
+    override fun toString(): String {
+        return serialization
+    }
 }
 
 object Types {
     val Str = ParameterType.Single("String")
     val Number = ParameterType.Single("Number")
     val CommaDelimitedList = ParameterType.Single("CommaDelimitedList")
-}
-
-
-class ParameterTypeSerializer : StdSerializer<ParameterType>(ParameterType::class.java) {
-    override fun serialize(value: ParameterType?, gen: JsonGenerator?, provider: SerializerProvider?) {
-        gen?.writeString(value?.serialization)
-    }
-
 }
