f4643292bb03f87e88cfd84799cb7b6e80b5ee03
Lloyd Ramey <lnr0626@gmail.com>
2017-02-12 00:08:14 -0500

    Allow specifying a string name without val

--- base-models/src/main/kotlin/com/lloydramey/cfn/model/Output.kt
+++ base-models/src/main/kotlin/com/lloydramey/cfn/model/Output.kt
@@ -2,7 +2,10 @@ package com.lloydramey.cfn.model
 
 import com.fasterxml.jackson.annotation.JsonIgnore
 import com.lloydramey.cfn.model.functions.AwsTemplateValue
+import com.lloydramey.cfn.model.functions.Val
 
-data class Export(val name: AwsTemplateValue)
+data class Export(val name: AwsTemplateValue) {
+    constructor(name: String) : this(Val(name))
+}
 
 class Output(@JsonIgnore val name: String, val value: AwsTemplateValue, val description: String? = null, val export: Export? = null)
