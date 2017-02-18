96148e9b136de8ad712cee627cb15de1498eacef
Lloyd Ramey <lnr0626@gmail.com>
2017-02-17 19:53:27 -0500

    Updated Outputs to have conditions

--- base-models/src/main/kotlin/com/lloydramey/cfn/model/Output.kt
+++ base-models/src/main/kotlin/com/lloydramey/cfn/model/Output.kt
@@ -15,12 +15,23 @@
  */
 package com.lloydramey.cfn.model
 
+import com.fasterxml.jackson.annotation.JsonAnyGetter
 import com.fasterxml.jackson.annotation.JsonIgnore
 import com.lloydramey.cfn.model.functions.AwsTemplateValue
 import com.lloydramey.cfn.model.functions.Val
+import com.lloydramey.cfn.model.resources.ConditionalOn
 
 data class Export(val name: AwsTemplateValue) {
     constructor(name: String) : this(Val(name))
 }
 
-class Output(@JsonIgnore val name: String, val value: AwsTemplateValue, val description: String? = null, val export: Export? = null)
+data class Output(
+    @JsonIgnore val name: String,
+    @JsonIgnore val condition: ConditionalOn? = null,
+    val value: AwsTemplateValue,
+    val description: String? = null,
+    val export: Export? = null
+) {
+    @JsonAnyGetter
+    fun json() = if(condition != null) mapOf("Condition" to condition) else emptyMap()
+}
