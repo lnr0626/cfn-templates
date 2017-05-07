d8a35c87b0d1cd68dd8e98cf93c5c14f42dccfe1
Lloyd Ramey <lnr0626@gmail.com>
2017-05-07 17:18:17 -0400

    Updated to gradle 3.5 release

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,12 @@
-
-import nl.javadude.gradle.plugins.license.LicensePlugin
-import org.gradle.api.publish.maven.plugins.MavenPublishPlugin
 import org.gradle.api.tasks.JavaExec
+import org.gradle.script.lang.kotlin.apply
 import org.gradle.script.lang.kotlin.dependencies
 import org.gradle.script.lang.kotlin.get
 import org.gradle.script.lang.kotlin.gradleScriptKotlin
 import org.gradle.script.lang.kotlin.invoke
 import org.gradle.script.lang.kotlin.kotlinModule
-import org.gradle.script.lang.kotlin.license
-import org.gradle.script.lang.kotlin.plugin
 import org.gradle.script.lang.kotlin.repositories
 import org.gradle.script.lang.kotlin.version
-import org.jetbrains.kotlin.gradle.plugin.KotlinPluginWrapper
 
 buildscript {
     repositories {
@@ -24,13 +19,15 @@ buildscript {
 
 plugins {
     id("com.github.hierynomus.license") version "0.13.1"
+    id("com.jfrog.bintray") version ("1.7.3") apply (false)
+    id("nebula.kotlin") version ("1.1.2") apply (false)
 }
 
 subprojects {
     apply {
-        plugin<MavenPublishPlugin>()
-        plugin<KotlinPluginWrapper>()
-        plugin<LicensePlugin>()
+        plugin("nebula.kotlin")
+        plugin("com.github.hierynomus.license")
+        plugin("com.jfrog.bintray")
     }
 
     group = "com.lloydramey.cfn"
@@ -43,8 +40,6 @@ subprojects {
 
         strictCheck = false
         skipExistingHeaders = true
-
-        //TODO: Configure to use current year
     }
 
     configurations.create("ktlint")
@@ -75,3 +70,25 @@ subprojects {
     }
 
 }
+
+/**
+ * Retrieves or configures the [license][nl.javadude.gradle.plugins.license.LicenseExtension] project extension.
+ */
+fun Project.`license`(configure: nl.javadude.gradle.plugins.license.LicenseExtension.() -> Unit = {}) =
+    extensions.getByName<nl.javadude.gradle.plugins.license.LicenseExtension>("license").apply { configure() }
+
+
+/**
+ * Retrieves or configures the [downloadLicenses][nl.javadude.gradle.plugins.license.DownloadLicensesExtension] project extension.
+ */
+fun Project.`downloadLicenses`(configure: nl.javadude.gradle.plugins.license.DownloadLicensesExtension.() -> Unit = {}) =
+    extensions.getByName<nl.javadude.gradle.plugins.license.DownloadLicensesExtension>("downloadLicenses").apply { configure() }
+
+
+/**
+ * Retrieves or configures the [bintray][com.jfrog.bintray.gradle.BintrayExtension] project extension.
+ */
+fun Project.`bintray`(configure: com.jfrog.bintray.gradle.BintrayExtension.() -> Unit = {}) =
+    extensions.getByName<com.jfrog.bintray.gradle.BintrayExtension>("bintray").apply { configure() }
+
+
